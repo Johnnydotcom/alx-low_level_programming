427423c5bb69e922ae2323052423d651026ef021
Johnnydotcom <mr.qtshotsho@gmail.com>
2023-07-13 14:54:17 -0700

    6-size.c fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * description - C program that prints sizes of various types on the computer it is compiled and run on
+ * description - C program that prints sizes of various types
  *
  * Return: Always 0 (Success)
  */
