5c9a6c982b03acf34ad6242ed6505357537660e6
Johnnydotcom <mr.qtshotsho@gmail.com>
2023-07-17 14:07:10 -0700

    program prints last number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,28 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/*
+ * main- Entry point
+ *
+ * description - Programprints last number of a number
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
+	int digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	digit = n % 10;
+	
+	if (digit > 5)
+	printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	else if (digit == 0)
+	printf("Last digit of %d is %d and is 0\n", n, digit);
+	else if(digit < 6 && digit != 0)
+	printf("Last digit of %d is %d and is less than 6 but not 0\n", n, digit);
 	return (0);
 }
