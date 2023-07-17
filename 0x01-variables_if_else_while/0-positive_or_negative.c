673fdf3ded1d2111c552810daefc25ab9cb64ef8
Johnnydotcom <mr.qtshotsho@gmail.com>
2023-07-17 12:18:04 -0700

    Prints whether random number generated is negative, positive or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/*
+ * main - Entry point
+ *
+ * description - Code that ptints whether number is positive, negative or zero
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+        int n;
+        
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (n == 0)
+        printf("%i is zero, n");
+        else
+                if ( n > 0)
+                        printf("%i is positive, n");
+                else
+                        printf("%i is negative, n");
+        return (0);
+}
