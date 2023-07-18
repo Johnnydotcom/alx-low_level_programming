e7bdf9ad70fe52cec8d51d656a3b001bc26697dc
Johnnydotcom <mr.qtshotsho@gmail.com>
2023-07-18 15:57:56 -0700

    print alphabet x10 fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,7 +15,7 @@ int print_alphabet_x10(void)
 	char alpha;
 	int count = 0;
 
-	while (count <= 9; count++)
+	while (count++ <= 9)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 			_putchar(alpha);
