dc15a4a72d05f5eac70caf12636493693fd37a35
Johnnydotcom <mr.qtshotsho@gmail.com>
2023-07-18 17:15:50 -0700

    check if c is an int or char

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * @c - checks if c is a letter lowercase or upper
+ *
+ * Return: 1 if c is a letter else 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	for (c >= 'a'; c <= 'z' && c >= 'A'; c => 'Z')
+		return (1);
+	else 
+		return (0);
+}
