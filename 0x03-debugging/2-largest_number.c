9b5b9479cd30a7c09eb2846c9d38e2962c362ec2
Johnnydotcom <mr.qtshotsho@gmail.com>
2023-07-21 05:59:47 -0700

    finding largest number of  degub

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 2 numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b)
+{
+	int largest;
+
+	if (a > b)
+	{
+	largest = a;
+	else (b > a && a > c)
+	{
+		largest = b;
+	return (largest);
+}
