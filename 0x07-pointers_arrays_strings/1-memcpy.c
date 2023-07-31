966e0434f57a570c4b446e252a98d6aae56ee383
Johnnydotcom <mr.qtshotsho@gmail.com>
2023-07-31 13:35:11 -0700

    strchr, find first occurence of character

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,9 +3,9 @@
 /**
  * _memcpy - copies memory area
  * @dest - memory area to copied too
- * @src - to be copied
- * @n - to be copied
- *
+ * @src - memory area to be copied
+ * @n - number of bytes to be copied
+ *i
  * Return: (dest)
  */
 
@@ -13,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	while (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0-memset.c - function fills memory with a constant byte.
-1-memcpy.c - a function that copies memory area.
+1-memcpy.c - function that copies memory area.
+2-strchr.c - function locate a character in a string.
