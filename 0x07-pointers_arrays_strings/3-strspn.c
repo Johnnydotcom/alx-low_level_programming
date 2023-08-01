511a9722944ecc91dd82c0984377f73a09f7a9e8
Johnnydotcom <mr.qtshotsho@gmail.com>
2023-08-01 12:35:33 -0700

    3-strspn.c function that gets the length of a prefix substring.

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring.
+ *
+ * @s: string to be scanned
+ * @accept: string containing the characters to match
+ *
+ * Return: (s), length of prefix substring
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	char c;
+
+	while (c = 0; c != s)
+	{
+		c++;
+	}
+	return (s);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0-memset.c - function fills memory with a constant byte.
 1-memcpy.c - function that copies memory area.
 2-strchr.c - function locate a character in a string.
+3-strspn.c - a function that gets the length of a prefix substring.
